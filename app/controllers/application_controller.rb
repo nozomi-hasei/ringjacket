e5cb1c9d60f33d930c3568acc9ac6dbbcea696cc
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-09-22 23:59:16 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def basic_auth
@@ -7,4 +8,12 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :phone_number])
+  end
 end
+
+
+
+
