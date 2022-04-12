e3bd19c6506475cef2bc090c21f034f82d6bebd8
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2022-04-12 14:45:28 +0900

    top update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,13 +1,9 @@
-# frozen_string_literal: true
+class CustomFailure < Devise::FailureApp
+  def redirect_url
+    new_user_registration_path
+  end
+end
 
-# Assuming you have not yet modified this file, each configuration option below
-# is set to its default value. Note that some are commented out while others
-# are not: uncommented lines are intended to protect your configuration from
-# breaking changes in upgrades (i.e., in the event that future versions of
-# Devise change the default values for those options).
-#
-# Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
