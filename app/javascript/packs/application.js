d5c1380e300b42e53f7ebe07d49c646470d3aceb
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2022-04-15 12:58:15 +0900

    レイアウト　update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+$(window).on('load resize', function() {
+  var windowWidth = window.innerWidth;
+  var elements = $('#fixed-area');
+  if (windowWidth >= 768) {
+    Stickyfill.add(elements);
+  }else{
+    Stickyfill.remove(elements);
+  } 
+});
