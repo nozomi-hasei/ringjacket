c307aad231571c60aaf12b3a6aca4897b5fbeef3
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2022-04-15 14:35:23 +0900

    js update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,12 +9,3 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-$(window).on('load resize', function() {
-  var windowWidth = window.innerWidth;
-  var elements = $('#fixed-area');
-  if (windowWidth >= 768) {
-    Stickyfill.add(elements);
-  }else{
-    Stickyfill.remove(elements);
-  } 
-});
